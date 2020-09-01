360b5131b6809bec2f0ca13164237b2180c4ead0
jveselkov <jveselkov@bk.ru>
2020-09-01 18:39:12 +0500

    final commit

--- src/components/Table/Pagination.tsx
+++ src/components/Table/Pagination.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Pagination: React.FC<Props> = ({
   current,
   max,
-  onChange,
+  onChange = () => {},
 }: Props) => {
   const disableFirst: boolean = useMemo(() => current === 1, [
     current,
